7d30a24e210dc54b4113c7b09573f8cea48137c9
vtanda01 <vtanda01@etudiant.univ-lr.fr>
2024-03-14 23:24:31 +0100

    data init

--- back/includes/db_connect.php
+++ back/includes/db_connect.php
@@ -1,19 +1,19 @@
 <?php
 // Chemin vers votre fichier de base de données SQLite
 
-
+echo 'its works';
 /**
  * __DIR__ est une constante magique en PHP qui représente le chemin absolu
  *  du répertoire du fichier dans lequel elle est utilisée
  */
-$db_file = __DIR__ . '../db.sqlite';
+// $db_file = __DIR__ . '../db.sqlite';
 
-try {
-    // Connexion à la base de données SQLite avec PDO
-    $pdo = new PDO('sqlite:' . $db_file);
-    // Configuration de PDO pour lever des exceptions en cas d'erreur
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    // Gestion des erreurs de connexion
-    die('Erreur de connexion à la base de données : ' . $e->getMessage());
-}
+// try {
+//     // Connexion à la base de données SQLite avec PDO
+//     $pdo = new PDO('sqlite:' . $db_file);
+//     // Configuration de PDO pour lever des exceptions en cas d'erreur
+//     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// } catch (PDOException $e) {
+//     // Gestion des erreurs de connexion
+//     die('Erreur de connexion à la base de données : ' . $e->getMessage());
+// }
